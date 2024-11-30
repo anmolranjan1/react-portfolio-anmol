ced569c9fd3151f874f60d28ce7599414a0bf947
Anmol Ranjan <anmolranjan1@gmail.com>
2024-11-30 23:12:34 +0530

    Updated role

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
                             initial="hidden"
                             animate="visible"
                             className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
-                                iOS App Developer
+                                Software Developer
                         </motion.span>
                         <motion.p 
                             variants={container(0.8)}
